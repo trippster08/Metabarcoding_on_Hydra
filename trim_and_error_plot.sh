3476e858e2a0dee23289d7a188f408bd94ba3539
trippster08 <trippster08@outlook.com>
2024-09-06 10:51:10 -0400

    updated shell script to submit first job
    updated the shell script to submit the first job, which trims primers using cutadapt and create quality plots using DADA2 in R

--- trim_and_error_plot.sh
+++ trim_and_error_plot.sh
@@ -1,16 +1,51 @@
 # /bin/sh
 
-raw=$"1"
-
-# Please change "Path_to_forward_primer_including_primer_file" to the path of
-# your forward primer, and "Path_to_reverse_primer_including_primer_file" to 
-# the path of your reverse primer. Don't use relative paths here, for simplicity
-primerF="Path_to_forward_primer_including_primer_file"
-primerR="Path_to_reverse_primer_including_primer_file"
-# Please change 
+raw="$1"
+gene="$2"
+data=${raw}/../
 
 
+if
+  [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
+then  
+  echo "Correct path to raw read files not entered (*.fastq.gz)"
+  exit
+fi
 
-qsub -o logs/${raw}_cutadapt.log \
-  -N ${raw}_fastp \
-cutadapt.job ${primerF} ${primerR}
+if
+  [[ -z $2 ]]
+then
+  echo "Genetic code not entered (should be a number between 1 and 25)"
+  exit
+fi
+
+if
+  [[ ${gene} == "COI" || ${gene} == "COX1" || ${gene} == "CO1" ]]
+then
+  primerF="/scratch/nmnh_lab/macdonaldk/primers/COImlIntF_spacers.fas" && \
+  primerR="/scratch/nmnh_lab/macdonaldk/primers/jgCOIR_spacers.fas"
+else
+  if
+    [[ ${gene} == "12S" || ${gene} == "MiFish" ]]
+  then
+    primerF="/scratch/nmnh_lab/macdonaldk/primers/MiFish_12SF_spacers.fas" && \
+    primerR="/scratch/nmnh_lab/macdonaldk/primers/MiFish_12SR_spacers.fas"
+  else
+    if
+      [[ ${gene} == "18S" ]]
+    then
+      primerF="/scratch/nmnh_lab/macdonaldk/primers/18SF_spacers.fas" && \
+      primerR="/scratch/nmnh_lab/macdonaldk/primers/18SR_spacers.fas"
+    else
+      echo 'Incorrect reference database. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
+      exit
+    fi
+  fi    
+fi
+
+mkdir ${data}/trimmed_reads
+
+
+qsub -o logs/cutadapt.log \
+  -N cutadapt \
+trim.job ${data} ${primerF} ${primerR}
