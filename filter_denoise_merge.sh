ccb0645077eafa5d03074ba5b580c2e4f6d70d41
trippster08 <trippster08@outlook.com>
2024-09-09 19:59:16 -0400

    added checks for called variables, renamed submitted job

--- filter_denoise_merge.sh
+++ filter_denoise_merge.sh
@@ -2,13 +2,28 @@
 trimmed="$1"
 truncF="$2"
 truncR="$3"
-data=${trimmed}/../../
 
+if
+  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
+then  
+  echo "Correct path to trimmed read files not entered (*.fastq.gz)"
+  exit
+fi
 
-
-
+if
+  [[ -z $2 ]]
+then
+  echo "Length to truncate forward reads not entered (should be a number > 1)"
+  exit
+fi
+if
+  [[ -z $3 ]]
+then
+  echo "Length to truncate reverse reads not entered (should be a number > 1)"
+  exit
+fi
 
 
 qsub -o logs/denoise.log \
   -N denoise \
-trim.job ${trimmed} ${truncF} ${truncR} ${data}
+filter_denoise_merge.job ${trimmed} ${truncF} ${truncR}
