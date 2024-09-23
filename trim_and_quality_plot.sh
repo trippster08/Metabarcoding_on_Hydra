d39ff3cf664a305f8595e0d2a9a7e8cfde2ffd8b
trippster08 <trippster08@outlook.com>
2024-09-23 16:28:18 -0400

    fixed path to primers

--- trim_and_quality_plot.sh
+++ trim_and_quality_plot.sh
@@ -22,20 +22,20 @@ fi
 if
   [[ ${gene} == "COI" || ${gene} == "COX1" || ${gene} == "CO1" ]]
 then
-  primerF=${data}"../primers/COImlIntF_spacers.fas" && \
-  primerR=${data}"../primers/jgCOIR_spacers.fas"
+  primerF=${data}"../Metabarcoding_on_Hydra-main/primers/COImlIntF_spacers.fas" && \
+  primerR=${data}"../Metabarcoding_on_Hydra-main/primers/jgCOIR_spacers.fas"
 else
   if
     [[ ${gene} == "12S" || ${gene} == "MiFish" ]]
   then
-    primerF=${data}"../primers/MiFish_12SF_spacers.fas" && \
-    primerR=${data}"../primers/MiFish_12SR_spacers.fas"
+    primerF=${data}"../Metabarcoding_on_Hydra-main/primers/MiFish_12SF_spacers.fas" && \
+    primerR=${data}"../Metabarcoding_on_Hydra-main/primers/MiFish_12SR_spacers.fas"
   else
     if
       [[ ${gene} == "18S" ]]
     then
-      primerF=${data}"..//primers/18SF_spacers.fas" && \
-      primerR=${data}"..//primers/18SR_spacers.fas"
+      primerF=${data}"../Metabarcoding_on_Hydra-main/primers/18SF_spacers.fas" && \
+      primerR=${data}"../Metabarcoding_on_Hydra-main/primers/18SR_spacers.fas"
     else
       echo 'Incorrect reference database. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
       exit
