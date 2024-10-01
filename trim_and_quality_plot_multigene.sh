0c07f425f651fdb60bef909b67bceba73eab86a6
trippster08 <trippster08@outlook.com>
2024-10-01 09:57:29 -0400

    Fixed a lot of things
    Also added download of pipeline and moving of pipeline files and primer directory. Also fixed path to primers. Also added reverse-complement primer sequences for 12S, and fixed statements looking at input and using those primers as variables. Also reduced the number of possible primer set names for simplicity of the code

--- trim_and_quality_plot_multigene.sh
+++ trim_and_quality_plot_multigene.sh
@@ -5,6 +5,12 @@ gene1="$2"
 gene2="$3"
 data=${raw}/../
 
+wget https://github.com/trippster08/Metabarcoding_on_Hydra/archive/refs/heads/main.zip
+
+unzip main.zip
+mv Metabarcoding_on_Hydra-main/* .
+mv primers ..
+rm -r Metabarcoding_on_Hydra-main
 
 if
   [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
@@ -15,54 +21,33 @@ fi
 
 # Check if either variable is empty
 if [ -z ${gene1} ] || [ -z ${gene2} ]; then
-  echo "One or both primers missing. Please include two primer pairs, possibilities include "COI", "COX1", "CO1", "12S", "MiFish", or "18S"."
+  echo "One or both primer sets missing. Please include two of the following three primer sets  "COI", "12S", or "18S"."
   exit 1
 fi
 
 
 if
-  [[ ${gene1} == "COI" || ${gene1} == "COX1" || ${gene1} == "CO1" ]]
+  [[ ${gene1} == "COI" || ${gene1} == "12S" && ${gene2} == "COI" || ${gene2} == "12S" ]]
 then
-  primer1F=${data}"../primers/COImlIntF_spacers.fas" && \
-  primer1R=${data}"../primers/jgCOIR_spacers.fas"
+  primerF=${data}"../primers/COImlIntF_12SFMiFish_spacers.fas" && \
+  primerR=${data}"../primers/jgCOIR_12SRMiFish_spacers.fas" && \
+  primerFrc=${data}"../primers/MiFish_12SF_RC_spacers.fas" && \
+  primerRrc=${data}"../primers/MiFish_12SR_RC_spacers.fas"
 else
   if
-    [[ ${gene1} == "12S" || ${gene1} == "MiFish" ]]
+  [[ ${gene1} == "18S" || ${gene1} == "12S" && ${gene2} == "18S" || ${gene2} == "12S" ]]
   then
-    primer1F=${data}"../primers/MiFish_12SF_spacers.fas" && \
-    primer1R=${data}"../primers/MiFish_12SR_spacers.fas"
+  primerF=${data}"../primers/18SF_12SFMiFish_spacers.fas" && \
+  primerR=${data}"../primers/18SR_12SRMiFish_spacers.fas" && \
+  primerFrc=${data}"../primers/MiFish_12SF_RC_spacers.fas" && \
+  primerRrc=${data}"../primers/MiFish_12SR_RC_spacers.fas"
   else
-    if
-      [[ ${gene1} == "18S" ]]
-    then
-      primer1F=${data}"..//primers/18SF_spacers.fas" && \
-      primer1R=${data}"..//primers/18SR_spacers.fas"
-    else
-      echo 'Incorrect primers referenced. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
-      exit
-    fi
-  fi    
-fi
-
-if
-  [[ ${gene2} == "COI" || ${gene2} == "COX1" || ${gene2} == "CO1" ]]
+  [[ ${gene1} == "COI" || ${gene1} == "18S" && ${gene2} == "COI" || ${gene2} == "18S" ]]
 then
-  primer2F=${data}"../primers/COImlIntF_spacers.fas" && \
-  primer2R=${data}"../primers/jgCOIR_spacers.fas"
-else
-  if
-    [[ ${gene2} == "12S" || ${gene2} == "MiFish" ]]
-  then
-    primer2F=${data}"../primers/MiFish_12SF_spacers.fas" && \
-    primer2R=${data}"../primers/MiFish_12SR_spacers.fas"
-  else
-    if
-      [[ ${gene2} == "18S" ]]
-    then
-      primer2F=${data}"..//primers/18SF_spacers.fas" && \
-      primer2R=${data}"..//primers/18SR_spacers.fas"
+  primerF=${data}"../primers/COImlIntF_18SF_spacers.fas" && \
+  primerR=${data}"../primers/jgCOIR_18SR_spacers.fas" &&
     else
-      echo 'Incorrect primers referenced. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
+      echo 'Incorrect primers referenced. Please enter "COI", "12S", or "18S"'
       exit
     fi
   fi    
