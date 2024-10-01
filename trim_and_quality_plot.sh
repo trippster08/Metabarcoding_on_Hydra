b5702b7de3eccae3231e4bf5c84bf12017674b36
trippster08 <trippster08@outlook.com>
2024-10-01 09:55:47 -0400

    Fixed carryover from mitofinder shell script
    Also added download of pipeline and moving of pipeline files and primer directory. Also fixed path to primers

--- trim_and_quality_plot.sh
+++ trim_and_quality_plot.sh
@@ -1,9 +1,15 @@
 # /bin/sh
 
 raw="$1"
-gene="$2"
+primer="$2"
 data=${raw}/../
 
+wget https://github.com/trippster08/Metabarcoding_on_Hydra/archive/refs/heads/main.zip
+
+unzip main.zip
+mv Metabarcoding_on_Hydra-main/* .
+mv primers ..
+rm -r Metabarcoding_on_Hydra-main
 
 if
   [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
@@ -15,29 +21,29 @@ fi
 if
   [[ -z $2 ]]
 then
-  echo "Genetic code not entered (should be a number between 1 and 25)"
+  echo "Primer set not given (Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S" after path to raw reads)"
   exit
 fi
 
 if
   [[ ${gene} == "COI" || ${gene} == "COX1" || ${gene} == "CO1" ]]
 then
-  primerF=${data}"../Metabarcoding_on_Hydra-main/primers/COImlIntF_spacers.fas" && \
-  primerR=${data}"../Metabarcoding_on_Hydra-main/primers/jgCOIR_spacers.fas"
+  primerF=${data}"../primers/COImlIntF_spacers.fas" && \
+  primerR=${data}"../primers/jgCOIR_spacers.fas"
 else
   if
     [[ ${gene} == "12S" || ${gene} == "MiFish" ]]
   then
-    primerF=${data}"../Metabarcoding_on_Hydra-main/primers/MiFish_12SF_spacers.fas" && \
-    primerR=${data}"../Metabarcoding_on_Hydra-main/primers/MiFish_12SR_spacers.fas"
+    primerF=${data}"../primers/MiFish_12SF_spacers.fas" && \
+    primerR=${data}"../primers/MiFish_12SR_spacers.fas"
   else
     if
       [[ ${gene} == "18S" ]]
     then
-      primerF=${data}"../Metabarcoding_on_Hydra-main/primers/18SF_spacers.fas" && \
-      primerR=${data}"../Metabarcoding_on_Hydra-main/primers/18SR_spacers.fas"
+      primerF=${data}"../primers/18SF_spacers.fas" && \
+      primerR=${data}"../primers/18SR_spacers.fas"
     else
-      echo 'Incorrect reference database. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
+      echo 'Incorrrect primer set given. Please enter "COI", "COX1", "CO1", "12S", "MiFish", or "18S"'
       exit
     fi
   fi    
