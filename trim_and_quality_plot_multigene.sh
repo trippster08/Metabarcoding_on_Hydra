ac3d5fcd1c3d192ea314f3620f3c73c5b10a7df9
trippster08 <trippster08@outlook.com>
2024-10-09 09:17:57 -0400

    lots of changes
    I simplified primer definition by setting up an array of primer set names and fixed how primers sets are determined.  I added output to let the user know what primers were selected. I changed the variables to be passed to the hydra job.

--- trim_and_quality_plot_multigene.sh
+++ trim_and_quality_plot_multigene.sh
@@ -1,16 +1,15 @@
-# /bin/sh
+# /bin/bash
 
 raw="$1"
 gene1="$2"
 gene2="$3"
 data=${raw}/../
-COI=("COI" "coi" "CO1" "co1" "cox1" "COX1")
-12S=("12S" "MiFish" "mifish" "Mifish" "12S_mifish" "12S_MiFish" "12s")
-18S=("18S" "l8s")
+COI=(COI coi CO1 co1 cox1 COX1)
+MiFish=(12S MiFish mifish Mifish 12S_mifish 12S_MiFish 12s)
+V4=(18S l8s V4 v4)
 
 if
-  [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
-then  
+  [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]; then  
   echo "Correct path to raw read files not entered (*.fastq.gz)"
   exit
 fi
@@ -21,31 +20,59 @@ if [ -z ${gene1} ] || [ -z ${gene2} ]; then
   exit 1
 fi
 
-if
-  [[ ${gene1} == ${COI} || ${gene1} == ${12S} && ${gene2} == ${COI} || ${gene2} == ${12S} ]]
-then
-  primerF=${data}"../primers/COImlIntF_12SFMiFish_spacers.fas" && \
-  primerR=${data}"../primers/jgCOIR_12SRMiFish_spacers.fas" && \
-  primerFrc=${data}"../primers/MiFish_12SF_RC_spacers.fas" && \
-  primerRrc=${data}"../primers/MiFish_12SR_RC_spacers.fas"
-else
-  if
-  [[ ${gene1} == ${18S} || ${gene1} == ${12S} && ${gene2} == ${18S} || ${gene2} == ${12S} ]]
-  then
-  primerF=${data}"../primers/18SF_12SFMiFish_spacers.fas" && \
-  primerR=${data}"../primers/18SR_12SRMiFish_spacers.fas" && \
-  primerFrc=${data}"../primers/MiFish_12SF_RC_spacers.fas" && \
-  primerRrc=${data}"../primers/MiFish_12SR_RC_spacers.fas"
-  else
-  [[ ${gene1} == ${COI} || ${gene1} == ${18S} && ${gene2} == ${COI} || ${gene2} == ${18S} ]]
-then
-  primerF=${data}"../primers/COImlIntF_18SF_spacers.fas" && \
-  primerR=${data}"../primers/jgCOIR_18SR_spacers.fas" &&
-    else
-      echo 'Incorrect primers referenced. Please enter "COI", "12S", or "18S"'
-      exit
+# Function to check if a value is in an array
+contains() {
+  local array="$1[@]"
+  local seeking=$2
+  local in=1
+  for x in "${!array}"; do
+    if [[ $x == $seeking ]]; then
+      in=0
+      break
     fi
-  fi    
+  done
+  return $in
+}
+
+if (contains COI "$gene1" && contains MiFish "$gene2") || (contains MiFish "$gene1" && contains COI "$gene2"); then
+  primerFpath=${data}"../primers/COImlIntF_12SFMiFish_spacers.fas"
+  primerRpath=${data}"../primers/jgCOIR_12SRMiFish_spacers.fas"
+  primerFrcpath=${data}"../primers/MiFish_12SF_RC_spacers.fas"
+  primerRrcpath=${data}"../primers/MiFish_12SR_RC_spacers.fas"
+  primerF=`basename ${primerFpath}`
+  primerR=`basename ${primerRpath}`
+  primerFrc=`basename ${primerFrcpath}`
+  primerRrc=`basename ${primerRrcpath}`
+  echo "Primer files set:"
+  echo "Forward primer file: `basename ${primerF}`"
+  echo "Reverse primer file: $primerR"
+  echo "Forward primer RC file: $primerFrc"
+  echo "Reverse primer RC file: $primerRrc"
+elif (contains V4 "$gene1" && contains MiFish "$gene2") || (contains V4 "$gene2" && contains MiFish "$gene1"); then
+  primerFpath=${data}"../primers/18SF_12SFMiFish_spacers.fas"
+  primerRpath=${data}"../primers/18SR_12SRMiFish_spacers.fas"
+  primerFrcpath=${data}"../primers/MiFish_12SF_RC_spacers.fas"
+  primerRrcpath=${data}"../primers/MiFish_12SR_RC_spacers.fas"
+  primerF=`basename ${primerFpath}`
+  primerR=`basename ${primerRpath}`
+  primerFrc=`basename ${primerFrcpath}`
+  primerRrc=`basename ${primerRrcpath}`
+  echo "Primer files set:"
+  echo "Forward primer file: `basename ${primerF}`"
+  echo "Reverse primer file: $primerR"
+  echo "Forward primer RC file: $primerFrc"
+  echo "Reverse primer RC file: $primerRrc"
+elif (contains COI "$gene1" && contains V4 "$gene2") || (contains COI "$gene2" && contains V4 "$gene1"); then
+  primerFpath=${data}"../primers/COImlIntF_18SF_spacers.fas"
+  primerRpath=${data}"../primers/jgCOIR_18SR_spacers.fas"
+  primerF=`basename ${primerFpath}`
+  primerR=`basename ${primerRpath}`
+  echo "Primer files set:"
+  echo "Forward primer file: $primerF"
+  echo "Reverse primer file: $primerR"
+else
+      echo 'No Primer files set. Incorrect primers referenced. Please enter "COI", "12S", or "18S"'
+      exit 1   
 fi
 
 mkdir -p \
@@ -53,7 +80,8 @@ ${data}/working/trimmed_reads/${gene1} \
 ${data}/working/filtered_reads/${gene1} \
 ${data}/working/trimmed_reads/${gene2} \
 ${data}/working/filtered_reads/${gene2} \
+${data}/working/trimmed_reads/mismatches \
 
 qsub -o logs/cutadapt.log \
   -N cutadapt \
-trim_and_quality_plot_multigene.job ${data} ${primerF} ${primerR} ${primerFrc} ${primerRrc} ${gene1} ${gene2} ${raw}
+trim_and_quality_plot_multigene.job ${data} ${gene1} ${gene2} ${primerF} ${primerR} ${primerFrc} ${primerRrc}
