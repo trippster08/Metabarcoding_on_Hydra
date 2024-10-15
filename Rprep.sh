3f452255e3a18fe9e1135329ab5ca24296b60853
trippster08 <trippster08@outlook.com>
2024-10-15 12:57:29 -0400

    created a file to load the most recent R module, then run Rprep.R

---
+++ Rprep.sh
@@ -0,0 +1,7 @@
+# This loads the most recent version of R, currently 4.1.1.
+
+module load tools/R/4.1.1
+
+# Rscript is a way of running R commands (here through an .R file, but it can
+# also run commands directly) without running R interactively
+Rscript Rprep.R
