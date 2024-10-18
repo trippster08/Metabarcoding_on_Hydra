bcf865f27b67145183251ba374d5e871b675e809
trippster08 <trippster08@outlook.com>
2024-10-18 08:37:41 -0400

    Whoops, wrong R version

--- Rprep.sh
+++ Rprep.sh
@@ -1,6 +1,6 @@
 # This loads the most recent version of R, currently 4.1.1.
 
-module load tools/R/4.1.1
+module load tools/R/4.4.1
 
 # Rscript is a way of running R commands (here through an .R file, but it can
 # also run commands directly) without running R interactively
