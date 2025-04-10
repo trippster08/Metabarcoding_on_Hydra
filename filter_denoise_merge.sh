3797167cb46f589c01e9ed65210cc94209da75a6
trippster08 <trippster08@outlook.com>
2025-04-10 12:24:07 -0400

    formatting

--- filter_denoise_merge.sh
+++ filter_denoise_merge.sh
@@ -12,7 +12,7 @@ fi
 
 if
   [[ ! ${truncF} =~ ^[0-9]+$ ]] || \
-    [[ ! ${truncR} =~ ^[0-9]+$ ]]
+  [[ ! ${truncR} =~ ^[0-9]+$ ]]
 then
   echo "At least one of the lengths-to-truncate values for (values should be a number > 1). Please enter '<path_to_trimmed_reads> <R1_truncation_value> <R2_truncation_value>" 
   exit
