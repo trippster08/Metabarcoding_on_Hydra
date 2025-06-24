73866fdebe3f5de07f4c8c6c5c92c5661f6e2a87
Tripp Macdonald <trippster08@outlook.com>
2025-06-23 20:59:30 -0400

    fixed relative path to everything

--- trim_and_quality_plot.sh
+++ trim_and_quality_plot.sh
@@ -2,14 +2,14 @@
 
 echo ${@}
 echo ${#}
-raw=$(readlink -f ../data/raw)
+raw=$(readlink -f data/raw)
 echo ${raw}
-data=$(readlink -f ../data)
+data=$(readlink -f data)
 echo ${data}
-primerF=$(readlink -f ../primers/primerF.fas)
-primerR=$(readlink -f ../primers/primerR.fas)
-primerFrc=$(readlink -f ../primers/primerF_RC.fas)
-primerRrc=$(readlink -f ../primers/primerR_RC.fas)
+primerF=$(readlink -f primers/primerF.fas)
+primerR=$(readlink -f primers/primerR.fas)
+primerFrc=$(readlink -f primers/primerF_RC.fas)
+primerRrc=$(readlink -f primers/primerR_RC.fas)
 # List of possible primers
 possible_primers=(COI 18S MiFish 16S 28SAnth 16Sbac)
 echo ${possible_primers[@]}
@@ -21,14 +21,14 @@ if ! ls "${raw}"/*.fastq.gz 2>/dev/null | grep -q fastq.gz; then
   exit 1
 fi
 
-: > ../primers/primerF.fas
-: > ../primers/primerR.fas
-: > ../primers/primerF_RC.fas
-: > ../primers/primerR_RC.fas
+: > primers/primerF.fas
+: > primers/primerR.fas
+: > primers/primerF_RC.fas
+: > primers/primerR_RC.fas
 
 if [ "$#" -eq 1 ]; then
   gene="$1"
-  path_to_trimmed=${data}"/working/trimmed_sequences/"${gene}
+  path_to_trimmed=${data}"/working/trimmed_sequences/"
   if [[ ! " ${possible_primers[@]} " =~ (^|[[:space:]])${gene}([[:space:]]|$) ]]; then
     echo "Error: ${gene} is not a valid primer name. Valid primer names are: ${possible_primers[@]}"
     exit 1
@@ -44,17 +44,17 @@ if [ "$#" -eq 1 ]; then
       echo "Trimming is already completed, we moving to the next step: 2_quality.job"
     else
         if [[ " ${RC_primers[*]} " == *" ${gene} "* ]];then
-          cp "../primers/${gene}F.fas" ../primers/primerF.fas
-          cp "../primers/${gene}R.fas" ../primers/primerR.fas
-          cp "../primers/${gene}F_RC.fas" ../primers/primerF_RC.fas
-          cp "../primers/${gene}R_RC.fas" ../primers/primerR_RC.fas
+          cp "primers/${gene}F.fas" primers/primerF.fas
+          cp "primers/${gene}R.fas" primers/primerR.fas
+          cp "primers/${gene}F_RC.fas" primers/primerF_RC.fas
+          cp "primers/${gene}R_RC.fas" primers/primerR_RC.fas
           qsub -o logs/cutadapt.log -N cutadapt \
-          1_trim.job ${#} ${@} ${data} ${primerF} ${primerR} ${primerFrc} ${primerRrc}
+          jobs/1_trim.job ${#} ${@} ${data} ${primerF} ${primerR} ${primerFrc} ${primerRrc}
         else
-          cp "../primers/${gene}F.fas" ../primers/primerF.fas
-          cp "../primers/${gene}R.fas" ../primers/primerR.fas
+          cp "primers/${gene}F.fas" primers/primerF.fas
+          cp "primers/${gene}R.fas" primers/primerR.fas
           qsub -o logs/cutadapt.log -N cutadapt \
-          1_trim.job ${#} ${@} ${data} ${primerF} ${primerR}
+          jobs/1_trim.job ${#} ${@} ${data} ${primerF} ${primerR}
         fi
     fi
   fi
@@ -63,7 +63,7 @@ elif [ "$#" -ge 2 ]; then
   if find "${path_to_trimmed}" -name "*.fastq.gz" -print -quit | grep -q .; then
     echo "Trimming is already completed, we are moving to the next step: 2_quality_multigene.job"
     qsub -o logs/quality_multigene.log -N quality \
-    2_quality_multigene.job ${[@]}
+    jobs/2_quality_multigene.job ${[@]}
   else
     for gene in "$@"; do
       if [[ ! " ${possible_primers[@]} " =~ (^|[[:space:]])${gene}([[:space:]]|$) ]]; then
@@ -73,17 +73,17 @@ elif [ "$#" -ge 2 ]; then
       fi
     done
     if [[ " ${RC_primers[@]} " =~ (^|[[:space:]])${gene}([[:space:]]|$) ]];then
-      cat "../primers/${gene}F.fas" >> ../primers/primerF.fas
-      cat "../primers/${gene}R.fas" >> ../primers/primerR.fas
-      cat "../primers/${gene}F_RC.fas" >> ../primers/primerF_RC.fas
-      cat "../primers/${gene}R_RC.fas" >> ../primers/primerR_RC.fas
+      cat "primers/${gene}F.fas" >> primers/primerF.fas
+      cat "primers/${gene}R.fas" >> primers/primerR.fas
+      cat "primers/${gene}F_RC.fas" >> primers/primerF_RC.fas
+      cat "primers/${gene}R_RC.fas" >> primers/primerR_RC.fas
       qsub -o logs/cutadapt.log -N cutadapt \
-      1_trim.job ${#} ${@} ${data} ${primerF} ${primerR} ${primerFrc} ${primerRrc}
+      jobs/1_trim.job ${#} ${@} ${data} ${primerF} ${primerR} ${primerFrc} ${primerRrc}
     else
-      cat "../primers/${gene}F.fas" >> ../primers/primerF.fas
-      cat "../primers/${gene}R.fas" >> ../primers/primerR.fas
+      cat "primers/${gene}F.fas" >> primers/primerF.fas
+      cat "primers/${gene}R.fas" >> primers/primerR.fas
       qsub -o logs/cutadapt.log -N cutadapt \
-      1_trim.job ${#} ${@} ${data} ${primerF} ${primerR}
+      jobs/1_trim.job ${#} ${@} ${data} ${primerF} ${primerR}
     fi
   fi  
 else
