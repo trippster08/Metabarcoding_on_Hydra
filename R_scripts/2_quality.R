60f3b42e632f098544c70fc6cab63d9a8c956d45
trippster08 <trippster08@outlook.com>
2025-06-26 15:11:34 -0400

    totally rewrote
    Rewrote this to work with any number of genes, from one to whatever. This simplifies the code a lot, and also makes it much more versitile

--- R_scripts/2_quality.R
+++ R_scripts/2_quality.R
@@ -1,5 +1,4 @@
 # Quality Plots ################################################################
-
 # We use DADA2 to obtain quality plots which we will use to filter in a later
 # section
 
@@ -12,14 +11,26 @@ suppressMessages(library(seqinr, warn.conflicts = FALSE, quietly = TRUE))
 suppressMessages(library(ShortRead, warn.conflicts = FALSE, quietly = TRUE))
 
 ## File Housekeeping ===========================================================
+genes <- commandArgs(trailingOnly = TRUE)
+num_genes <- length(genes)
+gene_list <- setNames(as.list(genes), genes)
+cat(
+  "These are the genes we will be creating quality plots for: ",
+  paste(genes, collapse = ", "),
+  "\n"
+)
+
 # Save project name as an object
-project_name <- basename(dirname(getwd()))
+project_name <- basename(getwd())
 print(paste0("This project is named ", project_name))
 
 # Set a path to the directory with the raw reads and cutadapt-trimmed reads.
-path_to_raw_reads <- "../data/raw"
-path_to_trimmed <- "../data/working/trimmed_sequences"
-
+path_to_raw_reads <- "data/raw"
+path_to_trimmed <- setNames(
+  lapply(genes, function(gene) paste0("data/working/trimmed_sequences/", gene)),
+  genes
+)
+path_to_results <- "data/results"
 ## Get Raw Read Counts =========================================================
 # Make a list of all the files in your "data/raw" folder.
 reads_to_trim <- list.files(path_to_raw_reads)
@@ -46,78 +57,139 @@ sequence_counts_raw <- sapply(
 # Name these counts with your sample names
 names(sequence_counts_raw) <- sample_names_raw
 
-print("Here are the raw read counts for each sample:")
+print(paste0("Here are the raw read counts for each sample:"))
 sequence_counts_raw
 
 ## Trimmed Reads ===============================================================
-# Create vectors for the trimmed reads, both forward (R1) and reverse (R2)
-trimmed_F <- sort(
-  list.files(
-    path_to_trimmed,
-    pattern = "_R1.fastq.gz",
-    full.names = TRUE
-  )
-)
-trimmed_R <- sort(
-  list.files(
-    path_to_trimmed,
-    pattern = "_R2.fastq.gz",
-    full.names = TRUE
-  )
+# Create vectors for the trimmed reads, both forward (R1) and reverse (R2) and
+# for gene1 and gene2.
+# Loop through each gene name
+
+# Make a list of all gene-specific trimmed reads (it's a list of 3 gene-specific
+# vectors containing trimmed read names)
+
+trimmed_reads <- setNames(
+  lapply(genes, function(gene) {
+    list(
+      F = sort(list.files(
+        path_to_trimmed[[gene]],
+        pattern = "_R1.fastq.gz",
+        full.names = TRUE
+      )),
+      R = sort(list.files(
+        path_to_trimmed[[gene]],
+        pattern = "_R2.fastq.gz",
+        full.names = TRUE
+      ))
+    )
+  }),
+  genes
 )
 
-# Make a new vector of sample names from your trimmed reads.
-sample_names_trimmed <- sapply(
-  strsplit(basename(trimmed_F), "_S\\d{1,3}_"),
-  `[`,
-  1
+sample_names_trimmed <- setNames(
+  lapply(genes, function(gene) {
+    files <- trimmed_reads[[gene]]$F
+    if (length(files) == 0 || is.null(files)) {
+      return(character(0))
+    }
+
+    sapply(
+      strsplit(basename(files), "_trimmed"),
+      `[`,
+      1
+    )
+  }),
+  genes
 )
+
 
 ## Remove empty sample files ===================================================
 # This saves the R1 fastq for the sample file only if both the R1 and R2 sample
 # files have reads.
-trimmed_noreads_F <- trimmed_F[sapply(trimmed_F, file.size) < 100]
-trimmed_noreads_R <- trimmed_R[sapply(trimmed_R, file.size) < 100]
 
-sample_names_trimmed_noreads <- sapply(
-  strsplit(basename(trimmed_noreads_F), "_S\\d{1,3}_"),
-  `[`,
-  1
-)
-names(trimmed_noreads_F) <- sample_names_trimmed_noreads
+# Create a new list to store non-empty trimmed reads
+actual_trimmed_reads <- setNames(vector("list", length(genes)), genes)
+sequence_counts_trimmed <- setNames(vector("list", length(genes)), genes)
 
-print(
-  "Here are the sample files for which contain no reads after primer trimming:"
-)
-names(trimmed_noreads_F)
 
-# Remove the empty files. invisible() does it without a logical (TRUE/FALSE)
-# message
-invisible(file.remove(trimmed_noreads_R))
-invisible(file.remove(trimmed_noreads_F))
+for (gene in genes) {
+  F_reads <- trimmed_reads[[gene]]$F
+  R_reads <- trimmed_reads[[gene]]$R
+  sample_names <- sample_names_trimmed[[gene]]
 
-# Remove zero read samples from trimmed_F amd trimmed_R vectors
-trimmed_exists_F <- trimmed_F[!trimmed_F %in% trimmed_noreads_F]
-trimmed_exists_R <- trimmed_R[!trimmed_R %in% trimmed_noreads_R]
-trimmed_F <- trimmed_exists_F
-trimmed_R <- trimmed_exists_R
+  # Count reads in R1 files
+  read_counts <- sapply(F_reads, function(file) {
+    fq <- readFastq(file)
+    length(fq)
+  })
 
-# Renew vector of sample names from your trimmed reads.
-sample_names_trimmed <- sapply(
-  strsplit(basename(trimmed_F), "_S\\d{1,3}_"),
-  `[`,
-  1
-)
+  # Name the read counts using sample_names_trimmed and store them
+  names(read_counts) <- sample_names
+  sequence_counts_trimmed[[gene]] <- read_counts
 
-nsamples <- length(sample_names_trimmed)
-print(paste(
-  "We will analyze",
-  nsamples,
-  "samples.",
-  sep = " "
-))
+  # Keep only files with at least 1 read
+  valid_indices <- which(read_counts > 0)
 
-## Make Quality Plots ==========================================================
+  actual_trimmed_reads[[gene]] <- list(
+    F = F_reads[valid_indices],
+    R = R_reads[valid_indices]
+  )
+}
+
+# Remove read files with zero reads for each gene
+for (gene in genes) {
+  original_samples <- sample_names_trimmed[[gene]]
+  kept_files <- actual_trimmed_reads[[gene]]$F
+
+  # Extract sample names from kept files
+  kept_samples <- if (length(kept_files) > 0) {
+    sapply(strsplit(basename(kept_files), "_trimmed"), `[`, 1)
+  } else {
+    character(0)
+  }
+
+  # Reset names for trimmed without removed samples
+  sample_names_trimmed <- setNames(
+    lapply(genes, function(gene) {
+      files <- actual_trimmed_reads[[gene]]$F
+      if (length(files) == 0 || is.null(files)) {
+        return(character(0))
+      }
+
+      sapply(
+        strsplit(basename(files), "_trimmed"),
+        `[`,
+        1
+      )
+    }),
+    genes
+  )
+
+  # Find filtered-out samples
+  removed_samples <- setdiff(original_samples, kept_samples)
+
+  # Print results
+  if (length(removed_samples) > 0) {
+    cat(
+      "\nThese samples had zero reads for this gene and were removed:",
+      gene,
+      "\n"
+    )
+    print(removed_samples)
+  } else {
+    cat("\nNo samples had zero reads for this gene:", gene, "\n")
+  }
+}
+
+#Count the number of samples remaining for each gene, and print
+for (gene in genes) {
+  nsamples <- length(sample_names_trimmed[[gene]])
+  message <- paste("We will analyze", nsamples, "samples for", gene)
+  print(message)
+}
+
+
+### Make Quality Plots ---------------------------------------------------------
 
 # This visualizes the quality plots. If you want to look at quality plots for
 # each individual sample, use "aggregate = FALSE", and include whichever sample
@@ -132,57 +204,96 @@ print(paste(
 # For these plots, the green line is the mean quality score at that position,
 # the orange lines are the quartiles (solid for median, dashed for 25% and 75%)
 # and the red line represents the proportion of reads existing at that position.
-quality_plot_F <- plotQualityProfile(
-  trimmed_F[1:length(sample_names_trimmed)],
-  aggregate = TRUE
-)
 
-plot_build <- ggplot_build(quality_plot_F)
-x_axis_range <- plot_build$layout$panel_params[[1]]$x.range
-max_x <- x_axis_range[2]
+quality_plots_F <- list()
+max_x_values <- list()
+quality_plots_F_better <- list()
 
-quality_plot_F_enhanced <- quality_plot_F +
-  scale_x_continuous(
-    limits = c(0, max_x),
-    breaks = seq(0, max_x, 10)
-  ) +
-  geom_vline(
-    xintercept = seq(0, max_x, 10),
-    color = "blue",
-    linewidth = 0.25
+for (gene in genes) {
+  forward_reads <- actual_trimmed_reads[[gene]]$F
+  sample_names <- sample_names_trimmed[[gene]]
+  # Make original plots
+  quality_plots_F[[gene]] <- plotQualityProfile(
+    forward_reads[1:length(sample_names)],
+    aggregate = TRUE
   )
+  # Extract x axis range and maximum x values
+  plot_build <- ggplot_build(quality_plots_F[[gene]])
+  x_axis_range <- plot_build$layout$panel_params[[1]]$x.range
+  max_x <- x_axis_range[2]
+  max_x_values[[gene]] <- max_x
 
-# Examine the reverse reads as you did the forward.
-quality_plot_R <- plotQualityProfile(
-  trimmed_R[1:length(sample_names_trimmed)],
-  aggregate = TRUE
-)
-quality_plot_R_enhanced <- quality_plot_R +
-  scale_x_continuous(
-    limits = c(0, max_x),
-    breaks = seq(0, max_x, 10)
-  ) +
-  geom_vline(
-    xintercept = seq(0, max_x, 10),
-    color = "blue",
-    linewidth = 0.25
+  # Enhance the plot
+  quality_plots_F_better[[gene]] <- quality_plots_F[[gene]] +
+    scale_x_continuous(
+      limits = c(0, max_x),
+      breaks = seq(0, max_x, 10)
+    ) +
+    geom_vline(
+      xintercept = seq(0, max_x, 10),
+      color = "blue",
+      linewidth = 0.25
+    ) +
+    annotate("text", x = 30, y = 2, label = paste0(gene, " R1"), size = 6)
+
+  # Save the plot as a PDF
+  ggsave(
+    filename = file.path(
+      path_to_results,
+      paste0(project_name, "_", gene, "_qualplotF_", gene, ".pdf")
+    ),
+    plot = quality_plots_F_better[[gene]],
+    width = 9,
+    height = 9
   )
+}
 
-# Export quality plots.
-ggsave(
-  paste0("../data/results/", project_name, "_qualplotF.pdf"),
-  plot = quality_plot_F_enhanced,
-  width = 9,
-  height = 9
-)
-ggsave(
-  paste0("../data/results/", project_name, "_qualplotR.pdf"),
-  plot = quality_plot_R_enhanced,
-  width = 9,
-  height = 9
-)
+# Do this for the reverse (R2) reads
 
-# Save all the objects created to this point in this section
-save.image(file = "../data/working/2_qual.RData")
+quality_plots_R <- list()
+max_x_values <- list()
+quality_plots_R_better <- list()
 
-print("Job 2_quality.job has finished")
+for (gene in genes) {
+  forward_reads <- actual_trimmed_reads[[gene]]$R
+  sample_names <- sample_names_trimmed[[gene]]
+  # Make original plots
+  quality_plots_R[[gene]] <- plotQualityProfile(
+    forward_reads[1:length(sample_names)],
+    aggregate = TRUE
+  )
+  # Extract x axis range and maximum x values
+  plot_build <- ggplot_build(quality_plots_R[[gene]])
+  x_axis_range <- plot_build$layout$panel_params[[1]]$x.range
+  max_x <- x_axis_range[2]
+  max_x_values[[gene]] <- max_x
+
+  # Enhance the plot
+  quality_plots_R_better[[gene]] <- quality_plots_R[[gene]] +
+    scale_x_continuous(
+      limits = c(0, max_x),
+      breaks = seq(0, max_x, 10)
+    ) +
+    geom_vline(
+      xintercept = seq(0, max_x, 10),
+      color = "blue",
+      linewidth = 0.25
+    ) +
+    annotate("text", x = 30, y = 2, label = paste0(gene, " R2"), size = 6)
+
+  # Save the plot as a PDF
+  ggsave(
+    filename = file.path(
+      path_to_results,
+      paste0(project_name, "_", gene, "_qualplotR_", gene, ".pdf")
+    ),
+    plot = quality_plots_R_better[[gene]],
+    width = 9,
+    height = 9
+  )
+}
+
+
+save.image(file = "data/working/2_qual.RData")
+
+print("Job 2_quality.job and this analysis has finished")
