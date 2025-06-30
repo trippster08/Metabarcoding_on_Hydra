81fd67cfd43b3ce717830122f1c9f14c07d4bfaf
trippster08 <trippster08@outlook.com>
2025-06-30 14:24:02 -0400

    Updated to work with any number of genes

--- R_scripts/3_filter.R
+++ R_scripts/3_filter.R
@@ -7,70 +7,54 @@ suppressMessages(library(seqinr, warn.conflicts = FALSE, quietly = TRUE))
 suppressMessages(library(ShortRead, warn.conflicts = FALSE, quietly = TRUE))
 
 ## File Housekeeping ===========================================================
+# Make objects to fill from job script
 args <- commandArgs(trailingOnly = TRUE)
-path_to_trimmed <- args[1]
-truncF <- as.numeric(args[2])
-truncR <- as.numeric(args[3])
+gene_num <- as.numeric(args[1])
+genes <- args[2:(gene_num + 1)]
+truncation_values <- as.integer(args[(gene_num + 2):length(args)])
+truncation_list <- setNames(
+  lapply(seq_len(gene_num), function(i) {
+    start <- (i - 1) * 2 + 1
+    end <- start + 1
+    truncation_values[start:end]
+  }),
+  genes
+)
+
 
 # Load the RData from "quality_plot_multigene.R"
-load("../data/working/2_qual.RData")
+load("data/working/2_qual.RData")
 
-## Trimed Sequences ============================================================
-# This creates a vector of the path for forward reads (R1, called trimmed_F).
-trimmed_F <- sort(list.files(
-  path_to_trimmed,
-  pattern = "_R1.fastq.gz",
-  full.names = TRUE
-))
 
-trimmed_R <- sort(list.files(
-  path_to_trimmed,
-  pattern = "_R2.fastq.gz",
-  full.names = TRUE
-))
-
-# Make a vector of sample names from your trimmed reads.
-sample_names_trimmed <- sapply(
-  strsplit(basename(trimmed_F), "_trimmed_"),
-  `[`,
-  1
-)
-
-# Give the vectors names
-names(trimmed_F) <- sample_names_trimmed
-names(trimmed_R) <- sample_names_trimmed
-
-## Get Read Counts of Trimmed Samples ==========================================
-# Count the number of reads in each trimmed sample. Since cutadapt only
-# keeps paired reads, we only need to count forward samples.
-sequence_counts_trimmed <- sapply(trimmed_F, function(file) {
-  fastq_data <- readFastq(file)
-  length(fastq_data)
-})
-names(sequence_counts_trimmed) <- sample_names_trimmed
-
-# This creates files for the reads that will be quality filtered with dada2
+# This creates file paths for the reads that will be quality filtered with dada2
 # in the next step.
-filtered_F <- file.path(
-  "../data/working",
-  "filtered_sequences",
-  paste0(
-    sample_names_trimmed,
-    "_F_filt.fastq.gz"
-  )
+filtered_reads <- setNames(vector("list", length(genes)), genes)
+path_to_filtered <- setNames(
+  lapply(genes, function(gene) {
+    paste0("data/working/filtered_sequences/", gene)
+  }),
+  genes
 )
-filtered_R <- file.path(
-  "../data/working",
-  "filtered_sequences",
-  paste0(
-    sample_names_trimmed,
-    "_R_filt.fastq.gz"
+for (gene in genes) {
+  sample_names_filtered <- sample_names_trimmed[[gene]]
+  filtered_reads[[gene]] <- list(
+    F = setNames(
+      file.path(
+        path_to_filtered,
+        paste0(sample_names_filtered, "_filt_F.fastq.gz")
+      ),
+      sample_names_filtered
+    ),
+    R = setNames(
+      file.path(
+        path_to_filtered,
+        paste0(sample_names_filtered, "_filt_R.fastq.gz")
+      ),
+      sample_names_filtered
+    )
   )
-)
+}
 
-# This inserts sample names to these newly created files.
-names(filtered_F) <- sample_names_trimmed
-names(filtered_R) <- sample_names_trimmed
 
 # This filters all reads depending upon the quality (as assigned by the user)
 # and trims the ends off the reads for all samples as determined by the quality
@@ -81,8 +65,8 @@ names(filtered_R) <- sample_names_trimmed
 # to multithread = FALSE, because "error messages and tracking are not handled
 # gracefully when using the multithreading functionality".
 
-# "truncLen=c(X,Y)" is how you tell Dada2 where to truncate all forward (X) and
-# reverse (Y) reads. Using "0" means reads will not be truncated.
+# "truncLen=c(i,j)" is how you tell Dada2 where to truncate all forward (i) and
+# reverse (j) reads. Using "0" means reads will not be truncated.
 # maxEE sets how many expected errors are allowed before a read is filtered out.
 
 # The amount to truncate is a common question, and very unsettled. I usually
@@ -90,74 +74,108 @@ names(filtered_R) <- sample_names_trimmed
 # reads) in the quality plot reaches 100%.
 
 # Most pipelines use a low maxEE (maximum number of expected errors), but I tend
-# to relax this value (from 0,0 to 4,4) because it increases the number of reads
+# to relax this value (from 0,0 to 6,6) because it increases the number of reads
 # that are kept, and Dada2 incorporates quality scores in its error models, so
 # keeping poorer-quality reads does not adversely effect the results, except in
 # very low quality reads. However, increasing maxEE does increase computational
 # time.
 
-filtered_summary <- filterAndTrim(
-  trimmed_F,
-  filtered_F,
-  trimmed_R,
-  filtered_R,
-  truncLen = c(truncF, truncR),
-  maxN = 0,
-  maxEE = c(4, 4),
-  rm.phix = TRUE,
-  truncQ = 2,
-  compress = TRUE,
-  multithread = TRUE,
-  verbose = TRUE
+# This step will create any new directories necessary (whatever path
+# filtered_reads uses).
+
+for (gene in genes) {
+  filterAndTrim(
+    actual_trimmed_reads[[gene]]$F,
+    filtered_reads[[gene]]$F,
+    actual_trimmed_reads[[gene]]$R,
+    filtered_reads[[gene]]$R,
+    truncLen = c(truncation_list[[gene]]),
+    maxN = 0,
+    maxEE = c(4, 4),
+    rm.phix = TRUE,
+    truncQ = 2,
+    compress = TRUE,
+    multithread = TRUE,
+    verbose = TRUE
+  )
+}
+
+# Save the reduced sample_names_filtered, since some samples no longer have
+# reads after filtering, and therefore no longer exist in the directory
+sample_names_filtered <- setNames(
+  lapply(genes, function(gene) {
+    files <- list.files(path_to_filtered[[gene]], pattern = "filt_F")
+    if (length(files) == 0 || is.null(files)) {
+      return(character(0))
+    }
+    sapply(strsplit(basename(files), "_filt_F"), `[`, 1)
+  }),
+  genes
 )
 
-# After filtering, if you have any samples with no reads, you much remove them.
-# This step changes filtered_F and filtered_R to only contain the names of
-# samples with reads. Do this only if there are samples in
-# "filtered_summary" with zero reads.
-exists <- file.exists(filtered_F) & file.exists(filtered_R)
-print(
-  "Here are the number of samples that were removed because they no longer contain reads after filtering"
-)
-length(filtered_F) - length(filtered_F[exists])
+# Update filtered_reads, since some samples no longer have
+# reads after filtering, and therefore no longer exist in the directory
+for (gene in genes) {
+  F_filtered <- list.files(
+    path_to_filtered[[gene]],
+    pattern = "_filt_F",
+    full.names = TRUE
+  )
+  R_filtered <- list.files(
+    path_to_filtered[[gene]],
+    pattern = "_filt_R",
+    full.names = TRUE
+  )
 
-filtered_F <- filtered_F[exists]
-filtered_R <- filtered_R[exists]
+  filtered_reads[[gene]] <- list(
+    F = setNames(
+      list.files(
+        path_to_filtered[[gene]],
+        pattern = "filt_F",
+        full.names = TRUE
+      ),
+      sapply(strsplit(basename(F_filtered), "_filt_F"), `[`, 1)
+    ),
+    R = setNames(
+      list.files(
+        path_to_filtered[[gene]],
+        pattern = "filt_R",
+        full.names = TRUE
+      ),
+      sapply(strsplit(basename(R_filtered), "_filt_R"), `[`, 1)
+    )
+  )
+}
 
+sequence_counts_filtered <- setNames(vector("list", length(genes)), genes)
 
-# Set a path to the directory with the dada2-filtered reads.
-path_to_filtered <- "../data/working/filtered_sequences"
+for (gene in genes) {
+  sequence_counts_filtered[[gene]] <- sapply(
+    filtered_reads[[gene]]$F,
+    function(file) {
+      fq <- readFastq(file)
+      length(fq)
+    }
+  )
+  sample_names <- sapply(
+    strsplit(basename(filtered_reads[[gene]]$F), "_filt_F"),
+    `[`,
+    1
+  )
+  names(sequence_counts_filtered[[gene]]) <- sample_names
+}
 
-# Get sample names for filtered reads
-sample_names_filtered <- sapply(
-  strsplit(basename(filtered_F), "_[FR]_filt"),
-  `[`,
-  1
-)
-
-# Count how many reads remain in each sample after filtering
-sequence_counts_filtered <- sapply(filtered_F, function(file) {
-  fastq_data <- readFastq(file)
-  length(fastq_data)
-})
-
-# Name the counts with sample names
-names(sequence_counts_filtered) <- sample_names_filtered
-
-print("Here are the read counts for each filtered sample:")
-sequence_counts_filtered
-
-# Export filtered_summary as a tsv
-write.table(
-  filtered_summary,
-  file = paste0("../data/results/", project_name, "_filtered_read_count.tsv"),
-  quote = FALSE,
-  sep = "\t",
-  row.names = TRUE,
-  col.names = NA
-)
+# Report removed samples
+for (gene in genes) {
+  removed <- setdiff(
+    sample_names_trimmed[[gene]],
+    sample_names_filtered[[gene]]
+  )
+  cat("\nHere are the samples removed after filtering for", gene, ":\n")
+  print(removed)
+}
 
 # Save all the objects created to this point in this section
-save.image(file = "../data/working/3_filter.RData")
+save.image(file = paste0("data/working/3_filter.RData"))
 
-print("Job 3_filter.job has finished")
+print("Job 3_filter_multigene.job and this analysis has finished")
