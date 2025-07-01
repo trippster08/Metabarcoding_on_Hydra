c573dffb3b32895795339d219d10839028fda340
Tripp Macdonald <trippster08@outlook.com>
2025-07-01 10:15:30 -0400

    fixed to consolidate single and multi-gene

--- R_scripts/4_error.R
+++ R_scripts/4_error.R
@@ -6,37 +6,48 @@ suppressMessages(library(tidyverse, warn.conflicts = FALSE, quietly = TRUE))
 suppressMessages(library(seqinr, warn.conflicts = FALSE, quietly = TRUE))
 suppressMessages(library(ShortRead, warn.conflicts = FALSE, quietly = TRUE))
 
-## Error Estimation ============================================================
+## File Housekeeping ===========================================================
+args <- commandArgs(trailingOnly = TRUE)
+gene_num <- as.numeric(args[1])
+genes <- args[2:(gene_num + 1)]
 
 # Load the RData from "quality_plot_multigene.R"
-load("../data/working/3_filter.RData")
+load("data/working/3_filter.RData")
+## Error Estimation ============================================================
 
 # Here we use a portion of the data to determine error rates. These error rates
 # will be used in the next (denoising) step to narrow down the sequences to a
 # reduced and corrected set of unique sequences
-errors_F <- learnErrors(
-  filtered_F,
-  nbases = 1e+08,
-  errorEstimationFunction = loessErrfun,
-  multithread = TRUE,
-  randomize = FALSE,
-  MAX_CONSIST = 10,
-  OMEGA_C = 0,
-  qualityType = "Auto",
-  verbose = FALSE
-)
+errors <- setNames(vector("list", length(genes)), genes)
 
-errors_R <- learnErrors(
-  filtered_R,
-  nbases = 1e+08,
-  errorEstimationFunction = loessErrfun,
-  multithread = TRUE,
-  randomize = FALSE,
-  MAX_CONSIST = 10,
-  OMEGA_C = 0,
-  qualityType = "Auto",
-  verbose = FALSE
-)
+for (gene in genes) {
+  errors[[gene]] <- list(F = NULL, R = NULL)
+
+  errors[[gene]]$F <- learnErrors(
+    filtered_reads[[gene]]$F,
+    nbases = 1e+08,
+    errorEstimationFunction = loessErrfun,
+    multithread = TRUE,
+    randomize = FALSE,
+    MAX_CONSIST = 10,
+    OMEGA_C = 0,
+    qualityType = "Auto",
+    verbose = FALSE
+  )
+  errors[[gene]]$R <- learnErrors(
+    filtered_reads[[gene]]$R,
+    nbases = 1e+08,
+    errorEstimationFunction = loessErrfun,
+    multithread = TRUE,
+    randomize = FALSE,
+    MAX_CONSIST = 10,
+    OMEGA_C = 0,
+    qualityType = "Auto",
+    verbose = FALSE
+  )  
+}
+
+
 
 # We can visualize the estimated error rates to make sure they don't look too
 # crazy. The red lines are error rates expected under the "...nominal defintion
@@ -46,22 +57,34 @@ errors_R <- learnErrors(
 # to look at here are to make sure that each black line is a good fit to the
 # observed error rates, and that estimated error rates decrease with increased
 # quality.
-error_plots_F <- plotErrors(errors_F, nominalQ = TRUE)
-error_plots_R <- plotErrors(errors_R, nominalQ = TRUE)
+error_plots <- setNames(vector("list", length(genes)), genes)
+for (gene in genes) {
+  error_plots[[gene]] <- list(F = NULL, R = NULL)
 
-ggsave(
-  paste0("../data/results/", project_name, "_error_plots_F.pdf"),
-  plot = error_plots_F,
-  width = 9,
-  height = 9
-)
-ggsave(
-  paste0("../data/results/", project_name, "_error_plots_R.pdf"),
-  plot = error_plots_R,
-  width = 9,
-  height = 9
-)
+  for (direction in c("F", "R")) {
+    err = errors[[gene]][[direction]]
+    error_plots[[gene]][[direction]] <- plotErrors(err, nominalQ = TRUE)
+    ggsave(
+      filename = file.path(
+        path_to_results,
+        paste0(
+        project_name,
+        "_errorplots",
+        direction,
+        "_",
+        gene,
+        ".pdf"
+      ),
+      plot = error_plots[[gene]][[direction]],
+      width = 9,
+      height = 9
+      )
+    )
+  }
+}
+
+
 # Save all the objects created to this point in this section
-save.image(file = "../data/working/4_error.RData")
+save.image(file = "data/working/4_error.RData")
 
-print("Job 3_error.job has finished")
+print("Job 4_error_multigene.job and this analysis has finished")
