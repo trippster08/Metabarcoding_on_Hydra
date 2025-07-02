0ccf74b7a84b677b36d7b64fd82c592368a77be4
trippster08 <trippster08@outlook.com>
2025-07-02 09:13:11 -0400

    fixed shell script to work for 1 or more genes

--- filter_denoise_merge.sh
+++ filter_denoise_merge.sh
@@ -1,46 +1,66 @@
-# /bin/sh
-trimmed=$(readlink -f ../data/working/trimmed_sequences)
-truncF="$1"
-truncR="$2"
+# /bin/bash
 
-if
-  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq)" ]]  
-then  
-  echo "No sequences (*.fastq.gz) were found in the trimmed data directory: ${trimmed}"
-  exit
+data=$(readlink -f data)
+path_to_results=${data}/results
+genes=()
+truncation_values=()
+possible_genes=(COI 18S MiFish 16S 28SAnth 16Sbac)
+# Loop through all arguments
+for arg in "$@"; do
+  if [[ $arg =~ ^[0-9]+$ ]]; then
+    # If the argument is a number, add it to the numbers array
+    numbers+=("$arg")
+  else
+    # If the argument is not a number, add it to the gene names array
+    gene_names+=("$arg")
+  fi
+done
+
+gene_num=${#genes}
+truncation_value_num={#truncation_values}
+
+if (( truncation_value_num < 2 * gene_num )); then
+  echo "There are not enough truncation values given, there should be two truncation values given for each gene."
+elif (( truncation_value_num > 2 * gene_num )); then
+  echo "There are two many trunction values given, there should be two truncation values given for each gene."
+else 
+  echo "Here are the genes you are analyzing, with their respective truncation values:"
+  for (( i=0; i<num_genes; i++ )); then
+    echo "${gene_names[i]} R1: ${numbers[2*i]} R2: ${numbers[2*i+1]}"
+  done
 fi
 
-if
-  [[ ! ${truncF} =~ ^[0-9]+$ ]] || \
-  [[ ! ${truncR} =~ ^[0-9]+$ ]]
-then
-  echo "Length to truncation values is not entered correctly. It must be a number between 0 (no trimming) and the length of your reads. Please enter <R1_truncation_value> <R2_truncation_value>" 
-  exit
-fi
+for gene in ${genes}; do
+  if [[ ! " ${possible_genes[@]} " =~ (^|[[:space:]])${gene}([[:space:]]|$) ]]; then
+    echo "Error: ${gene} is not a valid gene name. Valid genes are: ${possible_genes[@]}."
+    exit 1
+  fi
+done
 
-if [[ ! -f "../data/working/3_filter.RData" ]]; then
+
+if [[ ! -f "data/working/3_filter.RData" ]]; then
   qsub -o logs/filter.log -N filter \
-    3_filter.job ${trimmed} ${truncF} ${truncR}
-elif [[ ! -f "../data/working/4_error.RData" ]]; then
+    3_filter.job ${gene_num} ${genes} ${truncation_values}
+elif [[ ! -f "data/working/4_error.RData" ]]; then
   qsub -o logs/error.log -N error \
-    4_error.job ${trimmed} ${truncF} ${truncR}
+    4_error.job ${gene_num} ${genes}
   echo "Filtering is already complete; we will start with error modelling."
-elif [[ ! -f "../data/working/5_denoise.RData" ]]; then
+elif [[ ! -f "data/working/5_denoise.RData" ]]; then
   qsub -o logs/denoise.log -N denoise \
-    5_denoise.job ${trimmed} ${truncF} ${truncR}
+    5_denoise.job ${gene_num} ${genes}
   echo "Error modelling is already complete; we will start with denoising."
-elif [[ ! -f "../data/working/6_merge.RData" ]]; then
+elif [[ ! -f "data/working/6_merge.RData" ]]; then
   qsub -o logs/merge.log -N merge \
-    6_merge.job ${trimmed} ${truncF} ${truncR}
+    6_merge.job ${gene_num} ${genes}
   echo "Denoising is already complete; we will start with merging reads."
-elif [[ ! -f "../data/working/7_chimera.RData" ]]; then
+elif [[ ! -f "data/working/7_chimera.RData" ]]; then
   qsub -o logs/chimera.log -N chimera \
-    7_chimera.job ${trimmed} ${truncF} ${truncR}
-  echo "Merging is already complete; we will start with chimera removal."
-elif [[ ! -f "../data/working/8_output.RData" ]]; then
+    7_chimera.job ${gene_num} ${genes}
+  echo "Merging of reads is already complete; we will start with chimera removal."
+elif [[ ! -f "data/working/8_output.RData" ]]; then
   qsub -o logs/output.log -N output \
-  8_output.job ${trimmed} ${truncF} ${truncR}
+    8_output.job ${gene_num} ${genes}
   echo "Chimera removal is already complete; we will start with exporting results."
 else
-  echo "All steps have already completed"
+  echo "All steps for this analysis have already completed"
 fi
