af9997faad813fee3f9a3eee0d50107e54dda6d5
Tripp Macdonald <trippster08@outlook.com>
2026-08-27 07:58:19 -0400

    update assign taxonomy dada2 to work with multiple genes

--- assign_taxonomy_dada2.sh
+++ assign_taxonomy_dada2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 path_to_data=$(realpath ./data)
 project_name=$(basename "$PWD")
-gene="$@"
+genes="$@"
 
-qsub -o logs/tax_rdp_${gene}_hydra.log -N ${project_name}_${gene}_tax_rdp \
-jobs/9_assign_taxonomy_dada2_loop.job ${gene}
+for gene in ${genes}; do
+  qsub -o logs/tax_dada2_${gene}_hydra.log -N ${project_name}_${gene}_tax_dada2 \
+  jobs/9_assign_taxonomy_dada2_loop.job ${gene} ${path_to_data} ${project_name}
+  sleep 0.1
+done
